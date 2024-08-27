76723e4827768cf3db117ac2467ae6e75bf029d9
Lucas Carvalho <lucascf000@gmail.com>
2024-08-27 18:07:48 -0300

    Initial commit

---
+++ src/main/java/com/me/dominio_bancario/DominioBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.me.dominio_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioBancarioApplication.class, args);
+	}
+
+}
