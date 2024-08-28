448d6f0d17d182ae974a5bc98609027a1f2ffe6d
Lucas Carvalho <lucascf000@gmail.com>
2024-08-28 17:58:41 -0300

    Implementacao da camada de dominio

---
+++ src/main/java/com/me/dominio_bancario/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.me.dominio_bancario.domain.repository;
+
+import com.me.dominio_bancario.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
